f72d0b569b8fd6df322c6738df0f81833c849e93
surface74 <warwhisper@tut.by>
2022-05-02 01:29:37 +0300

    feat: done carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,19 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof(sampleActivity) !== 'string') {
+    return false;
+  }
+  let activity = Number.parseFloat(sampleActivity);
+
+  if (isNaN(activity) || (activity > MODERN_ACTIVITY) || (activity <= 0)) {
+    return false;
+  }
+
+  let age = 5730 * Math.log(MODERN_ACTIVITY / activity) / 0.693;
+
+  return Math.ceil(age);
 }
 
 module.exports = {
--- src/count-cats.js
+++ src/count-cats.js
@@ -16,6 +16,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function countCats(matrix) {
   let count = 0;
+  if (matrix.length === 0) {
+    return 0;
+  }
+
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
       if (matrix[i][j] === '^^') {
