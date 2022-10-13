167fdb72e4658bc01548136b1ae586c7e797f6cf
surface74 <warwhisper@tut.by>
2022-10-13 17:11:21 +0300

    feat: working src/extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -13,12 +13,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * repeater('STRING', { repeatTimes: 3, separator: '**', 
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
+ *     STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(string, { repeatTimes=1, separator = "+",
+  addition, additionRepeatTimes, additionSeparator="|" }) {
+  const str = arguments[0];
+  let options = Object.create(arguments[1]);
+
+  if (typeof(str) !== '')
+  options.repeatTimes = options.repeatTimes || 1;
+  options.separator = options.separator || "+";
+  options.additionSeparator = options.additionSeparator || "|";
+  
+  let additions = [];
+  for (let i = 0; i < options.additionRepeatTimes; i++) {
+    additions.push(options.addition);
+  }
+  console.log('additions: ', additions);
+
+  let mainPart = str + additions.join(options.additionSeparator);
+  
+  let mains = [];
+  for (let i = 0; i < options.repeatTimes; i++) {
+    mains.push(mainPart);
+  }
+  console.log('mains: ', mains);
+  console.log(mains.join(options.separator));
+  console.log('options.separator: ', options.separator);
+  console.log('options.additionSeparator: ', options.additionSeparator);
+  
+  return mains.join(options.separator);
 }
+
+repeater('STRING', {
+  repeatTimes: 3, separator: '**',
+  addition: 'PLUS', additionRepeatTimes: 3
+});
 
 module.exports = {
   repeater
